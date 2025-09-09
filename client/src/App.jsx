f1badf056015e9c8fda21c5349c3f153dad1f077
Zain Latif <zainlatif702@gmail.com>
2025-09-09 21:36:29 +0500

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Companies from "./components/Companies";
-import Categories from "./components/Categories";
 import Footer from "./components/Footer";
 
 
@@ -12,9 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Companies />
-      <Categories />
       <Footer />
-      {/* Other sections like Hero, Search box, etc */}
     </div>
   );
 }
