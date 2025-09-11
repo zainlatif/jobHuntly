605c86b7bad1cf021a75f5fbecc0bb6cac7aefe4
Zain Latif <zainlatif702@gmail.com>
2025-09-12 00:25:26 +0500

    fix css

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,14 +5,19 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>
-          Discover <br />
-          <span className="highlight">more than <span className="number">5000+ Jobs</span></span>
-        </h1>
-        <p>
-          Great platform for the job seeker that searching for new career heights 
-          and passionate about startups.
-        </p>
+        <div className="hero-title">
+          <h1>
+            Discover <br />
+            <span className="highlight">more than </span> <br />
+            <span className="number">5000+ Jobs</span>
+          </h1>
+          <img src="/assets/companies/blueline.svg" alt="blueline.svg" />
+          <p>
+            Great platform for the job seeker that searching for new career heights
+            and passionate about startups.
+          </p>
+        </div>
+
 
         {/* Search Box */}
         <div className="search-box">
@@ -23,7 +28,7 @@ const Hero = () => {
 
         {/* Popular keywords */}
         <p className="popular">
-          Popular: <span>UI Designer</span>, <span>UX Researcher</span>, 
+          Popular: <span>UI Designer</span>, <span>UX Researcher</span>,
           <span> Android</span>, <span> Admin</span>
         </p>
       </div>
